45d1d3602c951e3530f3d8a01565215e5eab5c0f
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2017-10-18 08:33:58 +0200

    Run rustfmt

--- squid/src/block_tokenizer.rs
+++ squid/src/block_tokenizer.rs
@@ -5,8 +5,8 @@ macro_rules! parse_line_starter {
     if $line.starts_with($starter) {
       return Some(Line::$variant(
         $line.chars()
-            .skip($starter.len())
-            .collect()
+             .skip($starter.len())
+             .collect()
       ))
     }
   }
@@ -14,73 +14,75 @@ macro_rules! parse_line_starter {
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum Line {
-  Blank,
-  Divider,
-  Heading1(String),
-  Heading2(String),
-  Heading3(String),
-  Text(String),
-  Quote(String),
-  Annotation(String),
-  UnorderedList(String),
-  OrderedList(String),
+    Blank,
+    Divider,
+    Heading1(String),
+    Heading2(String),
+    Heading3(String),
+    Text(String),
+    Quote(String),
+    Annotation(String),
+    UnorderedList(String),
+    OrderedList(String),
 }
 
 #[derive(Debug)]
 pub struct BlockTokenizer {
-  lines: VecDeque<String>,
+    lines: VecDeque<String>,
 }
 
 impl BlockTokenizer {
-  pub fn new<S: Into<String>>(input: S) -> Self {
-    // TODO: define behaviour around \r\n
-    let lines = input.into()
-                     .split('\n')
-                     .map(|line| line.to_string())
-                     .collect();
+    pub fn new<S: Into<String>>(input: S) -> Self {
+        // TODO: define behaviour around \r\n
+        let lines = input
+            .into()
+            .split('\n')
+            .map(|line| line.to_string())
+            .collect();
 
-    BlockTokenizer { lines }
-  }
-
-  pub fn consume_raw_line(&mut self) -> Option<String> {
-    self.lines.pop_front()
-  }
-
-  pub fn consume_line(&mut self) -> Option<Line> {
-    match self.consume_raw_line() {
-      None => { return None }
-      Some(line) => {
-        // TODO: optimize
-        let trimmed_line = line.trim().to_string();
-
-        if trimmed_line.len() == 0 {
-          return Some(Line::Blank);
-        }
-
-        parse_line_starter!(line, "# ", Heading1);
-        parse_line_starter!(line, "## ", Heading2);
-        parse_line_starter!(line, "### ", Heading3);
-        parse_line_starter!(line, "> ", Quote);
-        parse_line_starter!(line, "- ", UnorderedList);
-        parse_line_starter!(line, ". ", OrderedList);
-
-        if line.starts_with('[') && trimmed_line.ends_with(']') {
-          return Some(Line::Annotation(
-            trimmed_line.chars()
-                        .skip(1)
-                        .take(trimmed_line.len() - 2)
-                        .collect()
-          ));
-        }
-
-        if line.starts_with("---") && trimmed_line.chars().all(|c| c == '-') {
-          return Some(Line::Divider);
-        }
-
-        Some(Line::Text(line))
-      } 
+        BlockTokenizer { lines }
     }
-  }
+
+    pub fn consume_raw_line(&mut self) -> Option<String> {
+        self.lines.pop_front()
+    }
+
+    pub fn consume_line(&mut self) -> Option<Line> {
+        match self.consume_raw_line() {
+            None => return None,
+            Some(line) => {
+                // TODO: optimize
+                let trimmed_line = line.trim().to_string();
+
+                if trimmed_line.len() == 0 {
+                    return Some(Line::Blank);
+                }
+
+                parse_line_starter!(line, "# ", Heading1);
+                parse_line_starter!(line, "## ", Heading2);
+                parse_line_starter!(line, "### ", Heading3);
+                parse_line_starter!(line, "> ", Quote);
+                parse_line_starter!(line, "- ", UnorderedList);
+                parse_line_starter!(line, ". ", OrderedList);
+
+                if line.starts_with('[') && trimmed_line.ends_with(']') {
+                    return Some(Line::Annotation(
+                        trimmed_line
+                            .chars()
+                            .skip(1)
+                            .take(trimmed_line.len() - 2)
+                            .collect(),
+                    ));
+                }
+
+                if line.starts_with("---") && trimmed_line.chars().all(|c| c == '-') {
+                    return Some(Line::Divider);
+                }
+
+                Some(Line::Text(line))
+            }
+        }
+    }
 }
 
 #[cfg(test)]
@@ -99,7 +101,10 @@ mod tests {
     fn heading_1_works() {
         let mut tokenizer = BlockTokenizer::new("# hello\nworld");
 
-        assert_eq!(tokenizer.consume_line(), Some(Line::Heading1("hello".into())));
+        assert_eq!(
+            tokenizer.consume_line(),
+            Some(Line::Heading1("hello".into()))
+        );
         assert_eq!(tokenizer.consume_line(), Some(Line::Text("world".into())));
     }
 
@@ -107,21 +112,30 @@ mod tests {
     fn heading_2_works() {
         let mut tokenizer = BlockTokenizer::new("## heading 2");
 
-        assert_eq!(tokenizer.consume_line(), Some(Line::Heading2("heading 2".into())));
+        assert_eq!(
+            tokenizer.consume_line(),
+            Some(Line::Heading2("heading 2".into()))
+        );
     }
 
     #[test]
     fn heading_3_works() {
         let mut tokenizer = BlockTokenizer::new("### lorem ipsum");
 
-        assert_eq!(tokenizer.consume_line(), Some(Line::Heading3("lorem ipsum".into())));
+        assert_eq!(
+            tokenizer.consume_line(),
+            Some(Line::Heading3("lorem ipsum".into()))
+        );
     }
 
     #[test]
     fn text_with_hash_works() {
         let mut tokenizer = BlockTokenizer::new(" # lorem ipsum");
 
-        assert_eq!(tokenizer.consume_line(), Some(Line::Text(" # lorem ipsum".into())));
+        assert_eq!(
+            tokenizer.consume_line(),
+            Some(Line::Text(" # lorem ipsum".into()))
+        );
     }
 
     #[test]
@@ -129,7 +143,10 @@ mod tests {
         let mut tokenizer = BlockTokenizer::new("> quote\n > quote\n>quote");
 
         assert_eq!(tokenizer.consume_line(), Some(Line::Quote("quote".into())));
-        assert_eq!(tokenizer.consume_line(), Some(Line::Text(" > quote".into())));
+        assert_eq!(
+            tokenizer.consume_line(),
+            Some(Line::Text(" > quote".into()))
+        );
         assert_eq!(tokenizer.consume_line(), Some(Line::Text(">quote".into())));
     }
 
@@ -137,10 +154,22 @@ mod tests {
     fn annotation_works() {
         let mut tokenizer = BlockTokenizer::new("[code]\n[code]   \n [code] \n  [code]  \n[code");
 
-        assert_eq!(tokenizer.consume_line(), Some(Line::Annotation("code".into())));
-        assert_eq!(tokenizer.consume_line(), Some(Line::Annotation("code".into())));
-        assert_eq!(tokenizer.consume_line(), Some(Line::Text(" [code] ".into())));
-        assert_eq!(tokenizer.consume_line(), Some(Line::Text("  [code]  ".into())));
+        assert_eq!(
+            tokenizer.consume_line(),
+            Some(Line::Annotation("code".into()))
+        );
+        assert_eq!(
+            tokenizer.consume_line(),
+            Some(Line::Annotation("code".into()))
+        );
+        assert_eq!(
+            tokenizer.consume_line(),
+            Some(Line::Text(" [code] ".into()))
+        );
+        assert_eq!(
+            tokenizer.consume_line(),
+            Some(Line::Text("  [code]  ".into()))
+        );
         assert_eq!(tokenizer.consume_line(), Some(Line::Text("[code".into())));
     }
 
@@ -148,7 +177,10 @@ mod tests {
     fn unordered_list_works() {
         let mut tokenizer = BlockTokenizer::new("- item\n - item\n-item");
 
-        assert_eq!(tokenizer.consume_line(), Some(Line::UnorderedList("item".into())));
+        assert_eq!(
+            tokenizer.consume_line(),
+            Some(Line::UnorderedList("item".into()))
+        );
         assert_eq!(tokenizer.consume_line(), Some(Line::Text(" - item".into())));
         assert_eq!(tokenizer.consume_line(), Some(Line::Text("-item".into())));
     }
@@ -157,19 +189,25 @@ mod tests {
     fn ordered_list_works() {
         let mut tokenizer = BlockTokenizer::new(".  item\n . item\n.item");
 
-        assert_eq!(tokenizer.consume_line(), Some(Line::OrderedList(" item".into())));
+        assert_eq!(
+            tokenizer.consume_line(),
+            Some(Line::OrderedList(" item".into()))
+        );
         assert_eq!(tokenizer.consume_line(), Some(Line::Text(" . item".into())));
         assert_eq!(tokenizer.consume_line(), Some(Line::Text(".item".into())));
     }
 
     #[test]
     fn divider_works() {
-      let mut tokenizer = BlockTokenizer::new("---\n------- \n--\n ---\n---foobar");
+        let mut tokenizer = BlockTokenizer::new("---\n------- \n--\n ---\n---foobar");
 
-      assert_eq!(tokenizer.consume_line(), Some(Line::Divider));
-      assert_eq!(tokenizer.consume_line(), Some(Line::Divider));
-      assert_eq!(tokenizer.consume_line(), Some(Line::Text("--".into())));
-      assert_eq!(tokenizer.consume_line(), Some(Line::Text(" ---".into())));
-      assert_eq!(tokenizer.consume_line(), Some(Line::Text("---foobar".into())));
+        assert_eq!(tokenizer.consume_line(), Some(Line::Divider));
+        assert_eq!(tokenizer.consume_line(), Some(Line::Divider));
+        assert_eq!(tokenizer.consume_line(), Some(Line::Text("--".into())));
+        assert_eq!(tokenizer.consume_line(), Some(Line::Text(" ---".into())));
+        assert_eq!(
+            tokenizer.consume_line(),
+            Some(Line::Text("---foobar".into()))
+        );
     }
 }
